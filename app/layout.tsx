1c1ee0bdddc0e987c139afe1ca87dc63134bcb5c
Youssef Elezaby <youssefel3zaby@gmail.com>
2025-04-20 23:08:42 +0200

    initiate project

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "ai interviewer",
+  description: "An AI-powered platform for preparing for mock intervuews",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body className={`${monaSans.className} antialiased pattern`}>
+        {children}
+      </body>
+    </html>
+  );
+}
